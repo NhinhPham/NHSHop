c50309ec5d86407b15361cc7b791c393e74ec197
NhinhPham <phamthinhinh2802@gmail.com>
2024-04-08 10:00:29 +0700

    first commit

---
+++ src/home/Banner.tsx
@@ -0,0 +1,12 @@
+
+const Banner = () => {
+  return (
+    <div>
+        <section className="banner">
+            <img src="https://vietgigs.vn/public/storage/gigs/gallery/large/EC6D5D824FE2BE40EAB4.webp" alt="" className="banner__img" />
+        </section>
+    </div>
+  );
+}
+
+export default Banner
